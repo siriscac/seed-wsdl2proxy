68df90ee776acac095167ec027a966208b59f406
Muthuramakrishnan <mviswanathan@google.com>
2017-04-13 02:05:51 +0530

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var apigeetool = require('apigeetool')
+var gutil = require('gulp-util')
+var request = require('request');
+var fs = require('fs');
+var seedsdk = require('seed-sdk');
+var unzip = require('unzip');
+
+var PROXY_NAME = "WSDL to Proxy"
+var SAMPLE_NAME = "WSDL to Proxy"
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+var opts = {
+    organization: gutil.env.org,
+    token: gutil.env.token,
+    environments: gutil.env.env,    
+    environment: gutil.env.env,
+    wsdlUrl: gutil.env.wsdlUrl,
+    binding: gutil.env.binding,
+    debug: gutil.env.debug    
+}
+
+gulp.task('download-apiproxy', function() {
+    	
+});
+
+gulp.task('deploy',['download-apiproxy'], function(){
+    opts.api = PROXY_NAME
+    opts.proxies = PROXY_NAME
+    opts.environments = 'test'
+
+	var url = 'https://soap-to-rest.appspot.com/wsdl2proxy?wsdlUrl=' + opts.wsdlUrl + '&binding=' + opts.binding;
+
+	request(url)
+  		.pipe(fs.createWriteStream('apiproxy.zip'))
+  		.on('close', function () {
+   		 	console.log('File written!');
+			fs.createReadStream('apiproxy.zip').pipe(unzip.Extract({ path: '.' }));
+		
+	    	var sdk = apigeetool.getPromiseSDK()
+			return sdk.deployProxy(opts).then(function(){
+				console.log('success');
+			});
+  		});
+})
+
+gulp.task('clean',function(){
+	opts.api = PROXY_NAME
+    opts.proxies = PROXY_NAME
+    opts.environments = 'test'
+    
+    var sdk = apigeetool.getPromiseSDK()
+    return sdk.undeploy(opts)
+              .then(function(){ return sdk.delete(opts)})
+                console.log(app)
+
+})
